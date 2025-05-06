b12017c407f0717fa583805a525e51996d93f011
ltrang11 <ltrang1811@gmail.com>
2025-05-06 15:51:08 +0930

    last version

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const { httpLogger } = require('./middlewares/logger');
 
 // ==================== Routes =====================================
 const routes = require('./routes');
- // why this file know ./routes to connect with index.js
-
+ 
 // ==================== Global Middleware Setup ====================
 app.use(cors());
 app.use(helmet());
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -45,20 +45,3 @@ router.put('/payment/:userId',
 
 module.exports = router;
 
-
-// router.post(
-//     '/',
-//     body('name').notEmpty().withMessage('Name is required'),
-//     body('username').isEmail().withMessage('Username must be a valid email address'),
-//     body('username').notEmpty().withMessage('Username is required'),
-//     body('password').notEmpty().withMessage('Password is required'),
-//     body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
-//     (req, res, next) => {
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             return res.status(400).json({errors: errors.array()});
-//         }
-//         next();
-//     },
-//     userController.createUser
-// );
