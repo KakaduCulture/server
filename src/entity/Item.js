646551919ca1d69074591b9f69fe4abfc048b5b6
Daniel Tate <danieltate888@hotmail.com>
2025-06-23 18:54:06 +0930

    Optimized UI, topbar

--- src/entity/Item.js
+++ src/entity/Item.js
@@ -11,6 +11,7 @@ module.exports = new EntitySchema({
         },
         name: {
             type: "varchar",
+            nullable: true,
         },
         productId: {
             type: "int",
